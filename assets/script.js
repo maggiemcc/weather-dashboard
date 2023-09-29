11a1b21a52073413da62cac184dc0fcea4664e4a
Maggie McCausland <maggiemccausland@hotmail.com>
2023-09-29 16:18:54 -0600

    started fetch call

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var inputEl = document.querySelector('#search-input');
+
+var userSearch = 'london'
+
+function searchApi() {
+    // var fetchURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userSearch}&appid=6c3537dc7701b1d2d52795e48bb67bd3`
+
+    var fetchURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + userSearch + '&appid=6c3537dc7701b1d2d52795e48bb67bd3';
+    console.log(fetchURL)
+
+    fetch(fetchURL).then(function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+        return response.json();
+    })
+
+}
+
+searchApi()
+
