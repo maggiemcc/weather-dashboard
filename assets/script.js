26588b2aaf88ceb85340074faffa6ad3c0204795
Maggie McCausland <maggiemccausland@hotmail.com>
2023-09-30 11:23:53 -0600

    all data for current weather displays, added comments, moved forecastDay variable to inside if/else to fix error of getting wrong day after several searches

--- assets/script.js
+++ assets/script.js
@@ -3,24 +3,17 @@ var formEl = document.querySelector('#search-form');
 var weatherFiveDay = document.querySelector(".fiveDayWeather");
 var weatherToday = document.querySelector(".display-today");
 
-// CURRENT WEATHER: https://api.openweathermap.org/data/2.5/weather?
-
 // Create Array for days of the week
 var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 // Get day
 var day = new Date();
-// console.log("day", day)
-
 var dayNum = day.getDay();
-// console.log("dayNum", dayNum)
 
-var forecastDay = dayNum;
 
 // FETCH FIVE DAY FORECAST
 function searchFiveDayApi(userSearchVal) {
     var fetchURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + userSearchVal + '&appid=6c3537dc7701b1d2d52795e48bb67bd3&units=imperial';
-    console.log(fetchURL)
 
     fetch(fetchURL).then(function (response) {
         if (!response.ok) {
@@ -28,16 +21,19 @@ function searchFiveDayApi(userSearchVal) {
         }
         return response.json();
     }).then(function (data) {
-        console.log("5day >", data);
 
         if (!data.list.length) {
             console.log("no results");
         } else {
-            // Loop through to get each day
+
+            var forecastDay = dayNum;
+
+            // LOOP THOUGH TO GET EACH DAY
             for (var i = 0; i < data.list.length; i++) {
                 var time = data.list[i].dt_txt;
 
                 if (time.includes('03:00:00')) {
+                    // INSTEAD OF GRABING WEATHER FOR ONLY SATURDAY/FIRST FIVE OBJECTS, GETS ONE WEATHER OBJECT FOR EACH DAY
                     forecastDay += 1;
 
                     // Reset to first item of array if equal to last item in array
@@ -61,7 +57,7 @@ function searchFiveDayApi(userSearchVal) {
                     weatherIcon.src = iconPath;
                     placeDate.textContent = week[forecastDay].toUpperCase();
                     placeTemp.textContent = "Temp: " + data.list[forecastDay].main.temp + "ºF";
-                    placeWind.textContent = "Wind: " + data.list[forecastDay].wind.speed + "MPH";
+                    placeWind.textContent = "Wind: " + data.list[forecastDay].wind.speed + " MPH";
                     placeHumidity.textContent = "Humidity: " + data.list[forecastDay].main.humidity + "%";
 
 
@@ -85,7 +81,6 @@ function searchFiveDayApi(userSearchVal) {
 // FETCH TODAYS WEATHER
 function searchTodayApi(userSearchVal) {
     var fetchURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + userSearchVal + '&appid=6c3537dc7701b1d2d52795e48bb67bd3&units=imperial';
-    console.log(fetchURL)
 
     fetch(fetchURL).then(function (response) {
         if (!response.ok) {
@@ -93,17 +88,35 @@ function searchTodayApi(userSearchVal) {
         }
         return response.json();
     }).then(function (data) {
-        console.log("today >>", data);
 
         if (!data.weather.length) {
             console.log("no results");
         } else {
-            var placeName = document.createElement("h3");
+            // CREATE ELEMENTS
+            var placeName = document.createElement("h2");
             placeName.textContent = data.name;
+
+            var weatherIcon = document.createElement('img');
+            var iconCode = data.weather[0].icon;
+            var iconPath = "//openweathermap.org/img/w/" + iconCode + ".png";
+            weatherIcon.src = iconPath;
+
+            var placeTemp = document.createElement("h3");
+            placeTemp.textContent = "Temp: " + data.main.temp + "ºF";
+
+            var placeWind = document.createElement("h3");
+            placeWind.textContent = "Wind: " + data.wind.speed + " MPH";
+
+            var placeHumidity = document.createElement("h3");
+            placeHumidity.textContent = "Humidity: " + data.main.humidity + "%";
 
 
             // APPEND ITEMS
             weatherToday.appendChild(placeName);
+            weatherToday.appendChild(weatherIcon);
+            weatherToday.appendChild(placeTemp);
+            weatherToday.appendChild(placeWind);
+            weatherToday.appendChild(placeHumidity);
         }
     })
         .catch(function (error) {
@@ -121,8 +134,8 @@ function formSubmit(event) {
         return;
     }
 
-    weatherFiveDay.textContent = " ";
-    weatherToday.textContent = " ";
+    weatherFiveDay.innerHTML = " ";
+    weatherToday.innerHTML = " ";
     searchFiveDayApi(userSearchVal);
     searchTodayApi(userSearchVal);
 }
